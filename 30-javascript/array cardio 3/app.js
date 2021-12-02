7600ed96a31396c93e86b36b47f0c103fe6c64ad
tanishk15sharma <tanishk1582002@gmail.com>
2021-12-02 19:20:10 +0530

    sorting concept

--- 30-javascript/array cardio 3/app.js
+++ 30-javascript/array cardio 3/app.js
@@ -1,6 +1,29 @@
 const bands = ['The Plot in You', 'The Devil Wears Prada', 'Pierce the Veil', 'Norma Jean', 'The Bled', 'Say Anything', 'The Midway State', 'We Came as Romans', 'Counterparts', 'Oh, Sleeper', 'A Skylit Drive', 'Anywhere But Here', 'An Old Dog'];
 const names =   document.querySelector("#bands");
 
+console.log(bands);
+
+function strip (nameBand) {
+   return  nameBand.replace(/^(a |an |the)/i,"" ).trim();
+}
 
 
-console.log(names);
+const sortBands = bands.sort(function (a,b){
+    if( strip(a) >  strip(b)  ) {
+        return 1
+    }else{
+        return -1
+    }
+
+} )
+
+names.innerHTML = sortBands.map(item   => `<li>${item}</li>` ).join("")
+
+
+
+ console.log(sortBands);
+
+
+
+
+
